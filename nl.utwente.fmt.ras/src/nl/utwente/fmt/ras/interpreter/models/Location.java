b2396f59e871f2918f44b88819adf5759530deb9
Cyber-Carrot <107707387+Cyber-Carrot@users.noreply.github.com>
2023-02-15 12:36:56 +0000

    Small formatting fixes

--- nl.utwente.fmt.ras/src/nl/utwente/fmt/ras/interpreter/models/Location.java
+++ nl.utwente.fmt.ras/src/nl/utwente/fmt/ras/interpreter/models/Location.java
@@ -4,4 +4,25 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import nl.utw
+import nl.utwente.fmt.ras.ras.Card;
+import nl.utwente.fmt.ras.ras.CardLocation;
+import nl.utwente.fmt.ras.ras.LocationVisibility;
+
+public class Location {
+
+	private CardLocation loc;
+	private List<Card> cards = new ArrayList<Card>();
+
+	public Location(CardLocation loc) {
+		this.loc = loc;
+	}
+
+	public CardLocation getLoc() {
+		return loc;
+	}
+
+	public void setLoc(CardLocation loc) {
+		this.loc = loc;
+	}
+
+	public vo
