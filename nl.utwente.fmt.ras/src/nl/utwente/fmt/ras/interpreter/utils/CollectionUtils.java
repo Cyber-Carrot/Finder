0e53212aa3515846db38796ce5d912b29e2cc6c2
Cyber-Carrot <107707387+Cyber-Carrot@users.noreply.github.com>
2023-02-19 10:29:08 +0000

    chore(release): 0.4.0

--- nl.utwente.fmt.ras/src/nl/utwente/fmt/ras/interpreter/utils/CollectionUtils.java
+++ nl.utwente.fmt.ras/src/nl/utwente/fmt/ras/interpreter/utils/CollectionUtils.java
@@ -9,3 +9,29 @@ import java.util.Set;
 import org.eclipse.emf.common.util.EList;
 
 public class CollectionUtils {
+
+	private static Random r;
+
+	public static <T> List<T> intersection(List<T> list1, List<T> list2) {
+		List<T> list = new ArrayList<T>();
+
+		for (T t : list1) {
+			if (list2.contains(t)) {
+				list.add(t);
+			}
+		}
+
+		return list;
+	}
+
+	public static <T> T random(EList<T> list) {
+		Random rnd = r;
+
+		if (rnd == null)
+			r = rnd = new Random();
+
+		return list.get(rnd.nextInt(list.size()));
+	}
+
+	public static void shuffle(EList<?> list) {
+		Random rnd = r
